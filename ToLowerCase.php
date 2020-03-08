c63b71d43facda0e66603955de2689da67c4fad3
RubyTemple <alessiomorning1@gmail.com>
2020-03-09 00:00:58 +0100

    Update ToLowerCase.php

--- ToLowerCase.php
+++ ToLowerCase.php
@@ -3,7 +3,7 @@
 echo "Inizio\n";
 $files = scandir(getcwd());
 foreach($files as $file){
-	echo "Rinomino: ".$file."\nin: ".strtolower($file)."\n\n";
+	echo "Rinomino: ".$file."\nin: ".str_replace(' ','_',strtolower($file))."\n\n";
 	rename($file,str_replace(' ','_',strtolower($file)));
 }
 echo "\nFine";
